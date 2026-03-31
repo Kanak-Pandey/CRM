0a84f30679eed1d2ac13ab8444a6e9bc29fdecbb
Kanak-Pandey <kanakpandey302004@gmail.com>
2026-03-31 20:15:38 +0530

    .

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get('/',(req,res)=>{
 })
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
-
+console.log("REFRESH_CHECK_101");
 const PORT=process.env.PORT || 5000;
 
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
