0797e2a247435c3ec2fe4fe979bb5cd79ebf236f
Kanak-Pandey <kanakpandey302004@gmail.com>
2026-04-06 08:37:11 +0530

    fix prisma 7 config

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,15 +1,11 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { Pool } from '@neondatabase/serverless'
+import { PrismaClient } from '@prisma/client';
 
-// 👇 IMPORTANT CHANGE
-const connectionString ="postgresql://neondb_owner:npg_E9pIKSzwPD6O@ep-dark-mode-amhw8ozu-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+const globalForPrisma = globalThis;
 
-if (!connectionString) {
-  throw new Error("❌ No DB connection string found in ANY env variable");
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
 }
-
-const pool = new Pool({ connectionString })
-const adapter = new PrismaNeon(pool)
-
-export const prisma = new PrismaClient({ adapter })
