895cc6c4f76a0730012870a72ee65ce99693afa3
Kanak-Pandey <kanakpandey302004@gmail.com>
2026-04-06 08:24:24 +0530

    .

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,10 +3,7 @@ import { PrismaNeon } from '@prisma/adapter-neon'
 import { Pool } from '@neondatabase/serverless'
 
 // 👇 IMPORTANT CHANGE
-const connectionString =
-  process.env.DATABASE_URL ||
-  process.env.POSTGRES_URL ||
-  process.env.POSTGRES_PRISMA_URL;
+const connectionString ="postgresql://neondb_owner:npg_E9pIKSzwPD6O@ep-dark-mode-amhw8ozu-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 if (!connectionString) {
   throw new Error("❌ No DB connection string found in ANY env variable");
