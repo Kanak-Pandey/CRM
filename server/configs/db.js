895e858559f4f610a5d25dce63066254a65addf0
Kanak-Pandey <kanakpandey302004@gmail.com>
2026-04-11 17:12:40 +0530

    fix: use @prisma/adapter-pg for Prisma 7 compatibility

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,10 +1,15 @@
-// server/configs/db.js
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/client/index.js'
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
 
 const globalForPrisma = globalThis
 
-export const db = globalForPrisma.prisma || new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-})
+function createClient() {
+  const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+  const adapter = new PrismaPg(pool)
+  return new PrismaClient({ adapter })
+}
+
+export const db = globalForPrisma.prisma ?? createClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
